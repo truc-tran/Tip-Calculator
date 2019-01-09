ab4147e1e89692c13e7fba3b214c99e3c7da4e0f
Truc Tran <tructran@dhcp-v003-078.mobile.uci.edu>
2019-01-09 09:24:22 -0800

    Initial Commit

---
+++ tippy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tippy
+//
+//  Created by Truc Tran on 1/9/19.
+//  Copyright © 2019 Truc Tran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
